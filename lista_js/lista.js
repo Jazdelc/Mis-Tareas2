05854ffc0a14171ebd50c3a0a00177eacae3f0c6
Jazdelc <87844657+Jazdelc@users.noreply.github.com>
2022-11-13 00:07:56 -0300

    Add files via upload

---
+++ lista_js/lista.js
@@ -0,0 +1,15 @@
+const lista_datos = [
+    "Jazmin Sepulveda",
+     32,
+      true,
+       new Date(1990, 06, 19),
+
+       libro_favorito = {
+        titulo: "1984",
+        autor: "George Orwell",
+        fecha: new Date(1949, 0, 1),
+        url: "https://www.amazon.com/-/es/George-Orwell/dp/8499890946"
+       }
+];
+
+console.log(lista_datos);
