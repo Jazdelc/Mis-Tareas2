d21c0b0e9c9ca9a0a5dd7162d732691d223a02c1
Jazdelc <87844657+Jazdelc@users.noreply.github.com>
2022-11-13 00:09:28 -0300

    Add files via upload

---
+++ numeros/numeros.js
@@ -0,0 +1,17 @@
+let altura_cm = 160;
+console.log(altura_cm)
+
+let altura_mt = 1.60;
+console.log(altura_mt)
+
+let peso_kg = 55.5;
+console.log(peso_kg)
+
+let altura_max = Math.ceil(altura_mt)
+console.log(altura_max)
+
+let peso_min = Math.floor(peso_kg)
+console.log(peso_min)
+
+let igual = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(igual)
